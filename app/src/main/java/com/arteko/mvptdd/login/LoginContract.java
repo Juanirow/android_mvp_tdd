18030db544b78319ce6335d2a7f46bc029a4c94f
Juan Manuel Hernandez <juanma.hernandez.l@gmail.com>
2016-05-13 17:52:05 -0500

    se creo la el contract para los callbacks entre el presentador y la vista

---
+++ app/src/main/java/com/arteko/mvptdd/login/LoginContract.java
@@ -0,0 +1,15 @@
+package com.arteko.mvptdd.login;
+
+/**
+ * Created by juancho on 13/05/16.
+ */
+public class LoginContract {
+
+    public interface View {
+
+    }
+
+    public interface Actions{
+
+    }
+}
