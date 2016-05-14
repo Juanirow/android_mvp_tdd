5651927d2b355967ceeb063ea24a8c58ad54cb7c
Juan Manuel Hernandez <juanma.hernandez.l@gmail.com>
2016-05-14 00:15:05 -0500

    se agrego el metodo de login

--- app/src/main/java/com/arteko/mvptdd/login/LoginInteractor.java
+++ app/src/main/java/com/arteko/mvptdd/login/LoginInteractor.java
@@ -1,9 +1,31 @@
 package com.arteko.mvptdd.login;
 
+import android.os.Handler;
+
 /**
  * Created by juancho on 13/05/16.
  */
 public class LoginInteractor{
 
+    public interface OnLoginCallback {
 
+        void onSuccess();
+
+        void onError();
+    }
+
+    public void login(final String email, final String password, final OnLoginCallback callback){
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if(email.equals("email") && password.equals("password")){
+                    callback.onSuccess();
+                }
+                else{
+                    callback.onError();
+                }
+            }
+        }, 2000);
+    }
 }
